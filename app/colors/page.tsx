d221640d95578c2e6393383c5db987c0f2677b8f
Srijan Baniyal <john.rambo.9901@gmail.com>
2025-06-02 18:30:53 +0530

    Resetting things up

--- app/colors/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NavbarDemo } from "@/components/Navbar";
-
-export default function ColorsPage() {
-  return (
-    <>
-      <NavbarDemo />
-    </>
-  );
-}
--- app/docs/components/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NavbarDemo } from "@/components/Navbar";
-
-export default function ComponentsPage() {
-  return (
-    <>
-      <NavbarDemo />
-    </>
-  );
-}
